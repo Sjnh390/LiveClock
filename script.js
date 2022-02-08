e7294df85b61994fd849cb1fc06856b1c89a2181
Sjnh390 <pro.saadjnh@gmail.com>
2022-02-08 15:56:52 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const hourElement = document.querySelector('.hour');
+const minuteElement = document.querySelector('.minute');
+const secondElement = document.querySelector('.second');
+const timeElement = document.querySelector('.time');
+const dateElement = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "Jully", "August", "September", "October", "November", "December"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Night Mode'
+        
+    }
+    else{
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light Mode'
+    }
+})
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const hours = time.getHours()
+    const hourForClock = hours % 12 
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const date = time.getDate()
+    const year = time.getFullYear()
+
+
+    hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(hourForClock, 0, 11, 0, 360)}deg)`
+    minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeElement.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes }:${seconds < 10 ? `0${seconds}` : seconds }`
+    dateElement.innerHTML = `${days[day]} - ${months[month]} - ${date} - ${year}`
+    
+}
+
+// console.log(Date());
+
+
+function scale(num, in_min, in_max, out_min, out_max) {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime()
+
+setInterval(setTime, 1000)
